690165a531ac6750b3407fcda0c2cce605bafc6d
MortisMc <joemcafee@btinternet.com>
2022-05-12 22:14:22 +0100

    Suggestions now are hidden when appropriate

--- src/App/Interface/Interface.js
+++ src/App/Interface/Interface.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Styles from './Interface.module.css';
 
+// Simplify for string comparison
 function simplifyString(string){
   return string.replace(/\W/g, '').toLowerCase();
 }
@@ -11,12 +12,19 @@ function handleCorrect(countries, inputRef, setScore, setCountry, nextCountry){
   nextCountry(setCountry, countries);
 }
 
-function handleChange(event, country, countries, inputRef, setSuggestions, setScore, setCountry, nextCountry) {
+function handleInputChange(event, country, countries, inputRef, setSuggestions, setScore, setCountry, nextCountry) {
   const names = [];
   const input = event.target.value;
+  // Clears suggestions if input is empty
+  if (!input.length) {
+    setSuggestions(names);
+    return;
+  }
+  // Handles 'correct answer' scenario
   if (simplifyString(input) === simplifyString(country.name)) {
     handleCorrect(countries, inputRef, setScore, setCountry, nextCountry);
   }
+  // Displays relevent suggestions
   else {
     countries.forEach( x => {
       const name = x.name
@@ -30,15 +38,21 @@ export default function Interface({ country, countries, studyMode, setScore, set
   const [suggestions, setSuggestions] = useState([]);
   const inputRef = useRef();
   useEffect(() => {
-    inputRef.current.value = '';  
-  }, [studyMode])
+    inputRef.current.value = '';
+    setSuggestions([]);
+  }, [studyMode, setSuggestions]);
   
   return (
     <div className={Styles.component}>
-      <input ref={inputRef} onChange={(event) => handleChange(event, country, countries, inputRef, setSuggestions, setScore, setCountry, nextCountry)} type='text' placeholder='Type answer here' className='box'/>
-      <ul className={`${Styles.suggestions} box`}>
-        {suggestions.map( (x,i) => <li className={Styles.suggestion} key={i}>{x}</li> )}
-      </ul>
+      <input ref={inputRef} onChange={(event) => handleInputChange(event, country, countries, inputRef, setSuggestions, setScore, setCountry, nextCountry)} type='text' placeholder='Type answer here' className='box'/>
+      {
+      suggestions.length ?
+        <ul className={`${Styles.suggestions} box`}>
+          {suggestions.map( (x,i) => <li className={Styles.suggestion} key={i}>{x}</li> )}
+        </ul>
+      :
+        null
+      }
     </div>
   );
 }
